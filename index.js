55b944958d917ace81b17e9c4ca768e20fa15379
DESKTOP-MGAC9P3\Nevil <ndabre@myeffect.ca>
2019-03-17 14:05:59 -0400

    Greeting completed - english

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-module.exports = function greetingbot(string){
-    return string ? 'Hello '+ string.replace(/\s/g, ""): 'Hello good looking!';
+
+function getGreeting(name){
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    var guestName = name ? name.replace(/\s/g, ''): ' good looking!'
+    let greet;
+
+    if(hours<12){
+        greet = `Good Morning ${guestName}!`;
+    }else if(hours>=12 && hours <=17){
+        greet = `Good Afternoon ${guestName}!`;
+    }else if(hours>17 && hours <=24){
+        greet = `Good Night ${guestName}!`;
+    }else{
+        greet = `Good Day ${guestName}!`;
+    }
+
+    return greet;
+}
+
+
+module.exports = function greetingbot(name){
+    return getGreeting(name);
 }
